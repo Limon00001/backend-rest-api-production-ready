a6c140d3b721a5afa2445838ab0e8f92bf30556b
Limon00001 <limonhossain82@gmail.com>
2025-07-27 22:41:42 +0600

    feat(server-setup): setup initial server with typescript

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 27 Jul, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import express from 'express';
+
+// Express App
+const app = express();
+
+// Server Listening
+app.listen(5001, () => {
+  console.log('Server is running on http://localhost:5001');
+});
