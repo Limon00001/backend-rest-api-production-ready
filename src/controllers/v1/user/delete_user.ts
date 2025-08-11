5c0be82db7097f2d38a5dd8ef8cfadf04ef8b12e
Limon00001 <limonhossain82@gmail.com>
2025-08-11 21:36:56 +0600

    feat(server-user): implement user deletion with multiple blog cleanup functionality

--- src/controllers/v1/user/delete_user.ts
+++ src/controllers/v1/user/delete_user.ts
@@ -5,8 +5,12 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// External Imports
+import { v2 as cloudinary } from 'cloudinary';
+
 // Internal Imports
 import { logger } from '@/lib/winston';
+import { Blog } from '@/models/Blog';
 import { User } from '@/models/User';
 
 // Types
@@ -22,6 +26,36 @@ const deleteUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
 
   try {
+    /**
+     * Find all the blogs of the user
+     * Use lean() to return a plain JavaScript object instead of a Mongoose document
+     * This improves performance and reduces memory usage
+     */
+    const blogs = await Blog.find({ author: userId })
+      .select('banner.publicId')
+      .lean()
+      .exec();
+
+    // Get the public IDs of the user's blogs
+    const publicIds = blogs.map(({ banner }) => banner.publicId);
+
+    // Delete the user's blogs from Cloudinary
+    await cloudinary.api.delete_resources(publicIds);
+
+    // Log the deleted blog banner images from Cloudinary
+    logger.info('Multiple blog banner images deleted from Cloudinary', {
+      publicIds,
+    });
+
+    // Delete all the blogs of the user from the database
+    await Blog.deleteMany({ author: userId });
+
+    // Log the deleted blogs from the database
+    logger.info('Multiple blogs deleted successfully from the database', {
+      userId,
+      blogs,
+    });
+
     // Delete user by ID
     const user = await User.deleteOne({ _id: userId });
 
