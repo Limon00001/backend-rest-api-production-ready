6f42b88918d5dfc1af8e3fb08e4c9ca07e28f6b0
Limon00001 <limonhossain82@gmail.com>
2025-08-11 23:05:26 +0600

    feat(server-dislike): implement blog dislike functionality

--- src/routes/v1/like.ts
+++ src/routes/v1/like.ts
@@ -10,6 +10,7 @@ import { Router } from 'express';
 import { body, param } from 'express-validator';
 
 // Internal Imports
+import disLikeBlog from '@/controllers/v1/likes/dislike_blog';
 import likeBlog from '@/controllers/v1/likes/like_blog';
 import authenticate from '@/middlewares/authenticate';
 import authorize from '@/middlewares/authorize';
@@ -31,6 +32,20 @@ router.post(
     .withMessage('Invalid user ID'),
   validationErrors,
   likeBlog,
+);
+
+router.delete(
+  '/blog/:blogId',
+  authenticate,
+  authorize(['admin', 'user']),
+  param('blogId').isMongoId().withMessage('Invalid blog ID'),
+  body('userId')
+    .notEmpty()
+    .withMessage('User ID is required')
+    .isMongoId()
+    .withMessage('Invalid user ID'),
+  validationErrors,
+  disLikeBlog,
 );
 
 // Export
